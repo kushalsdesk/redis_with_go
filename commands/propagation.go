2261794d252ce1c3c615d182c7b1e87fd049a9b6
Kushal Mondal <kushal99mondal@gmail.com>
2025-09-06 21:58:05 +0530

    continue: Properly handled single/multi-replica propagation
    previously connection gets closed after handshake, now fixed to be open
    and process propagating commands across multi replicas as well

--- commands/propagation.go
+++ commands/propagation.go
@@ -20,15 +20,12 @@ var writeCommands = map[string]bool{
 	"DECR":   true,
 	"DECRBY": true,
 	"XADD":   true,
-	// Add more write commands as needed
 }
 
-// Check if a command should be propagated to replicas
 func IsWriteCommand(command string) bool {
 	return writeCommands[strings.ToUpper(command)]
 }
 
-// Convert command arguments to RESP array format
 func EncodeRESPArray(args []string) []byte {
 	if len(args) == 0 {
 		return []byte("*0\r\n")
@@ -36,10 +33,8 @@ func EncodeRESPArray(args []string) []byte {
 
 	var result strings.Builder
 
-	// Write array header
 	result.WriteString(fmt.Sprintf("*%d\r\n", len(args)))
 
-	// Write each argument as bulk string
 	for _, arg := range args {
 		result.WriteString(fmt.Sprintf("$%d\r\n%s\r\n", len(arg), arg))
 	}
@@ -47,42 +42,35 @@ func EncodeRESPArray(args []string) []byte {
 	return []byte(result.String())
 }
 
-// Propagate command to all connected replicas
 func PropagateCommand(args []string) {
 	if len(args) == 0 {
 		return
 	}
 
-	// Only propagate write commands
 	if !IsWriteCommand(args[0]) {
 		return
 	}
 
-	// Get current replication state
 	replState := store.GetReplicationState()
 	if replState.Role != "master" {
-		return // Only masters propagate commands
+		return
 	}
 
-	// Get all active replica connections
 	replicas := store.GetReplicaConnections()
 	if len(replicas) == 0 {
-		return // No replicas to propagate to
+		return
 	}
 
-	// Encode command as RESP array
 	respCommand := EncodeRESPArray(args)
 
 	fmt.Printf("Propagating command to %d replicas: %v\n", len(replicas), args)
 
-	// Send to all replicas
 	for _, replica := range replicas {
 		go func(r *store.ReplicationConnection) {
 			_, err := r.Connection.Write(respCommand)
 			if err != nil {
 				fmt.Printf("Failed to propagate command to replica %s: %v\n",
 					r.Address, err)
-				// Mark replica as disconnected
 				store.RemoveReplicaByConnection(r.Connection)
 			}
 		}(replica)
